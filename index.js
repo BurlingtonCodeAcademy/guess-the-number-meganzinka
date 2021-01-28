ad742ed25a1b5f66ecab7354bfa540a155a41588
meganzinka <76189342+meganzinka@users.noreply.github.com>
2021-01-28 18:29:24 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -6,13 +6,39 @@ function ask(questionText) {
     rl.question(questionText, resolve);
   });
 }
+console.log(`Let's play a game. Think of a number that is between 1 and 100. \nI will use my superior computation skills and guess your number in less than 7 tries!`)
 
 start();
 
 async function start() {
-  console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
-  let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
-  console.log('You entered: ' + secretNumber);
-  // Now try and complete the program.
+  //let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
+  let max = 100; 
+  let min = 1; 
+  let guess = Math.floor((max - min)/2); //most efficient guess 
+
+  //ask if my guess is right or wrong 
+  let yesOrNo = await ask(`Is your number ${guess}? Enter Y if yes or N if No.\n`);
+  //if yes, game is over 
+  if (yesOrNo.toUpperCase().trim() == "Y") {
+    console.log(`I KNEW your number was ${guess}!!!!`)
+    //bummer, I'm wrong! I need more info to figure out new max, min and guess 
+  } else { 
+    let higherOrLower = await ask(`Is your number higher or lower than ${guess}? Enter H for higher or L for lower.\n`);
+    while (higherOrLower.toUpperCase().trim() == 'H') {
+          // if person's number higher, new min should be last guess +1, max should be same, new guess computed 
+      min = guess + 1;
+    } 
+    while (higherOrLower.toUpperCase().trim =='L') {
+      //if person's number is lower, new max should be last guess - 1, min should be the same, new guess computed.
+      max = guess - 1;
+      let yesOrNo = await ask(`Is your number ${guess}? Enter Y if yes or N if No.\n`);
+    }
+
+  }
+
+  //console.log('You entered: ' + secretNumber);
+
   process.exit();
 }
+
+
